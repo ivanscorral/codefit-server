6a003cb62bb1d291ff11e90de01d656310416be4
Iván Sánchez <ramon.sanchez68@gmail.com>
2023-11-15 18:18:55 +0100

    feat: Add SetService skeleton
    - Created a new service layer file `SetService` with basic class structure.
    - The service is responsible for operations on Set objects such as create, get, update, and delete.
    - Yet to implement the actual functionality marked with TODOs.
    - `SetRepository` from the database layer is being injected via constructor.

---
+++ src/services/SetService.ts
@@ -0,0 +1,29 @@
+import { type Pool } from 'pg'
+import type Set from '../types/Set'
+import { SetRepository } from '../database/SetRepository'
+
+class SetService {
+  private readonly setRepository: SetRepository
+
+  constructor (pool: Pool) {
+    this.setRepository = new SetRepository(pool)
+  }
+
+  public async createSet (set: Set): Promise<void> {
+    // TODO: Implement set creation
+  }
+
+  public getSet (setId: number): Set | undefined {
+    // TODO: Implement set retrieval
+  }
+
+  public updateSet (setId: number, updatedSet: Set): Set | undefined {
+    // TODO: Implement set update
+  }
+
+  public deleteSet (setId: number): Set | undefined {
+    // TODO: Implement set deletion
+  }
+}
+
+export default SetService
